583c79f4871cdb2300158c9bce66260b869d0c76
ashjd <ashjd29@gmail.com>
2016-05-02 15:28:53 -0700

    Pass sending and fetching specs

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,52 @@
-// YOUR CODE HERE:
 
+
+var app = { 
+  server : 'https://api.parse.com/1/classes/messages'
+};
+
+app.init = function () {
+
+
+};
+
+app.send = function (message) {
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: app.server,
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+};
+
+app.fetch = function () {
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: app.server,
+    type: 'GET',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message received', data);
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to receive message');
+    }
+  });
+};
+
+app.clearMessages = function() {
+  $('#chats').empty();
+};
+
+app.addMessage = function (message) {
+  $('#chats').append(message);
+};
